abbdca1c5264ba612822ff9ce78a17d4b4ded473
budomihey <73472694+budomihey@users.noreply.github.com>
2021-02-08 16:40:00 +0300

    Add files via upload
    Кто пасётся на лугу?
    Заготовка для рагу

---
+++ New_progect.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+file = open('test_file.inc','r')
+
+inc=file.read()
+file.close()
+print(inc)
+FL = open('место где выводит зло программа.txt','r+')
+
+
